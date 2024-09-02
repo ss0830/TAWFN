3f1415bced61948d9ad87655077f39be3f84b10b
ss0830 <54929781+ss0830@users.noreply.github.com>
2024-09-02 21:48:04 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,6 @@ import copy
 import os
 
 CONFIG = ml_collections.ConfigDict({
-    "debug": False,
-    "max_epochs": 100,
-    "batch_size": 32,
-    "device": 'cuda:7',
-    "optimizer":{
-        "lr": 1e-4,
-        "betas": (0.95, 0.95),
-        "eps": 1e-8,
-    },
-    "scheduler":{
-        "step_size": 300,
-        "gamma": 0.75,
-    },
     "cnn_model_save_path": "model/MCNN/model_",
     "gcn_model_save_path": "model/AGCN/model_",
     # "model_save_path": "model/model_",
